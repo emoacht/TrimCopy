b178f3923cdcb5467fe0db1b10e7627099c9c7ad
emoact <emotom@pobox.com>
2017-02-19 15:56:36 +0930

    Updated license

--- Source/TrimCopy/Properties/AssemblyInfo.cs
+++ Source/TrimCopy/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TrimCopy")]
-[assembly: AssemblyCopyright("Copyright © 2016 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
