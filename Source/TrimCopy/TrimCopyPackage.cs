67c9919f3900c75b20c67ea54e6249c6cc221446
emoacht <emotom@pobox.com>
2019-12-15 09:36:27 +0900

    Modify to derive from AsyncPackage

--- Source/TrimCopy/TrimCopyPackage.cs
+++ Source/TrimCopy/TrimCopyPackage.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
+using Task = System.Threading.Tasks.Task;
 
 namespace TrimCopy
 {
@@ -29,36 +27,27 @@ namespace TrimCopy
 	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
 	/// </para>
 	/// </remarks>
-	[PackageRegistration(UseManagedResourcesOnly = true)]
-	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
 	[Guid("561c1f6f-729d-4159-b797-9e8c91c2f594")]
 	[ProvideOptionPage(typeof(SettingsPage), "Trim Copy", "General", 0, 0, true)]
 	[ProvideMenuResource("Menus.ctmenu", 1)]
-	public sealed class TrimCopyPackage : Package
+	public sealed class TrimCopyPackage : AsyncPackage
 	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="TrimCopyPackage"/> class.
-		/// </summary>
-		public TrimCopyPackage()
-		{
-			// Inside this method you can place any initialization code that does not require
-			// any Visual Studio service because at this point the package object is created but
-			// not sited yet inside Visual Studio environment. The place to do all the other
-			// initialization is the Initialize method.
-		}
-
-		#region Package Members
-
 		/// <summary>
 		/// Initialization of the package; this method is called right after the package is sited, so this is the place
 		/// where you can put all the initialization code that rely on services provided by VisualStudio.
 		/// </summary>
-		protected override void Initialize()
+		/// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+		/// <param name="progress">A provider for progress updates.</param>
+		/// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
 		{
-			base.Initialize();
+			// When initialized asynchronously, the current thread may be a background thread at this point.
+			// Do any initialization that requires the UI thread after switching to the UI thread.
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
 			TrimCopyCommand.Initialize(this);
 		}
-
-		#endregion
 	}
 }
