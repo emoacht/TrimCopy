e5e89a30266ae5bb939d4effbe5db094e6ee963d
emoacht <emotom@pobox.com>
2021-11-23 18:43:58 +0900

    Add shared project and move most files

---
+++ Source/TrimCopy.VSIX.Shared/SettingsPage.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+using TrimCopy.Models;
+
+namespace TrimCopy
+{
+	[Guid("206DA345-0703-4353-8AC8-D894B660410F")]
+	public class SettingsPage : DialogPage
+	{
+		[Category("Indent")]
+		[DisplayName("Tab size")]
+		[Description("Tab size for replacing leading tabs with spaces on copy. (1-8)")]
+		[DefaultValue(4)]
+		[TypeConverter(typeof(TabSizeConverter))]
+		public int TabSize { get; set; }
+
+		[Category("Indent")]
+		[DisplayName("Use tab size in Text Editor")]
+		[Description("Use tab size in Tabs of the respective language in Text Editor. If on, tab size here will be ignored.")]
+		[DefaultValue(Toggle.On)]
+		public Toggle UseTabSizeInTextEditor { get; set; }
+
+		[Category("Indent")]
+		[DisplayName("Fixed indent size")]
+		[Description("Fixed indent size for adjusting the leftmost indent on copy. (0-8)")]
+		[DefaultValue(4)]
+		[TypeConverter(typeof(IndentSizeConverter))]
+		public int FixedIndentSize { get; set; }
+
+		[Category("Supplement")]
+		[DisplayName("Trim trailing spaces")]
+		[Description("Trim trailing spaces on copy.")]
+		[DefaultValue(Toggle.On)]
+		public Toggle TrimTrailingSpaces { get; set; }
+
+		[Category("Supplement")]
+		[DisplayName("Use HTML encode")]
+		[Description("Use HTML encode on copy.")]
+		[DefaultValue(Toggle.Off)]
+		public Toggle UseHtmlEncode { get; set; }
+
+		[Category("Line Ending")]
+		[DisplayName("Line ending character")]
+		[Description("Line ending character to be used on copy.")]
+		[DefaultValue(LineEndType.CrLf)]
+		[TypeConverter(typeof(LineEndTypeConverter))]
+		public LineEndType LineEndType { get; set; }
+
+		#region event
+
+		private bool _isActivated = false;
+
+		protected override void OnActivate(CancelEventArgs e)
+		{
+			base.OnActivate(e);
+
+			if (!_isActivated)
+			{
+				_isActivated = true;
+
+				// Once a user made any change in this page, Visual Studio will create a key for 
+				// this extension under ApplicationPrivateSettings of the registry. After that,
+				// the values in the key will be automatically loaded when activated.
+				// Until then, default values of Settings class need to be reflected here.
+				Load();
+			}
+		}
+
+		protected override void OnApply(PageApplyEventArgs e)
+		{
+			base.OnApply(e);
+
+			Save();
+		}
+
+		#endregion
+
+		#region Load/Save
+
+		/// <summary>
+		/// Copies values of this extension's own settings to equivalent values of this page.
+		/// </summary>
+		private void Load()
+		{
+			TabSize = Settings.Current.TabSize;
+			UseTabSizeInTextEditor = Settings.Current.UseTabSizeInTextEditor ? Toggle.On : Toggle.Off;
+			FixedIndentSize = Settings.Current.FixedIndentSize;
+			TrimTrailingSpaces = Settings.Current.TrimTrailingSpaces ? Toggle.On : Toggle.Off;
+			UseHtmlEncode = Settings.Current.UseHtmlEncode ? Toggle.On : Toggle.Off;
+			LineEndType = Settings.Current.LineEndType;
+		}
+
+		/// <summary>
+		/// Copies values of this page to equivalent values of this extension's own settings and save to
+		/// the settings file.
+		/// </summary>
+		/// <remarks>
+		/// The values of this page will be automatically saved to the registry by Visual Studio.
+		/// </remarks>
+		private void Save()
+		{
+			Settings.Current.TabSize = TabSize;
+			Settings.Current.UseTabSizeInTextEditor = (UseTabSizeInTextEditor == Toggle.On);
+			Settings.Current.FixedIndentSize = FixedIndentSize;
+			Settings.Current.TrimTrailingSpaces = (TrimTrailingSpaces == Toggle.On);
+			Settings.Current.UseHtmlEncode = (UseHtmlEncode == Toggle.On);
+			Settings.Current.LineEndType = LineEndType;
+
+			Settings.Save();
+		}
+
+		#endregion
+	}
+
+	public class TabSizeConverter : Int32Converter
+	{
+		public override bool GetStandardValuesSupported(ITypeDescriptorContext context) => true;
+		public override bool GetStandardValuesExclusive(ITypeDescriptorContext context) => true;
+
+		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context) =>
+			new StandardValuesCollection(Enumerable.Range(1, 8).ToArray()); // 1-8
+	}
+
+	public class IndentSizeConverter : Int32Converter
+	{
+		public override bool GetStandardValuesSupported(ITypeDescriptorContext context) => true;
+		public override bool GetStandardValuesExclusive(ITypeDescriptorContext context) => true;
+
+		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context) =>
+			new StandardValuesCollection(Enumerable.Range(0, 9).ToArray()); // 0-8
+	}
+
+	public enum Toggle { On, Off }
+}
---
+++ Source/TrimCopy.VSIX.Shared/TrimCopyCommand.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Windows;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+using TrimCopy.Models;
+
+namespace TrimCopy
+{
+	/// <summary>
+	/// Command handler
+	/// </summary>
+	internal sealed class TrimCopyCommand
+	{
+		/// <summary>
+		/// Command IDs.
+		/// </summary>
+		/// <remarks>These values must match corresponding values in vsct file.</remarks>
+		public const int CopyOneCommandId = 0x0101;
+		public const int CopyTwoCommandId = 0x0102;
+
+		/// <summary>
+		/// Command menu group (command set GUID).
+		/// </summary>
+		public static readonly Guid CommandSet = new Guid("7bafd567-6a8c-4c9c-bbc9-438ffcd60e39");
+
+		/// <summary>
+		/// VS Package that provides this command, not null.
+		/// </summary>
+		private readonly Package package;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TrimCopyCommand"/> class.
+		/// Adds our command handlers for menu (commands must exist in the command table file)
+		/// </summary>
+		/// <param name="package">Owner package, not null.</param>
+		private TrimCopyCommand(Package package)
+		{
+			this.package = package ?? throw new ArgumentNullException(nameof(package));
+
+			if (this.ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+			{
+				var copyOneMenuCommandId = new CommandID(CommandSet, CopyOneCommandId);
+				var copyOneMenuCommand = new MenuCommand(this.ExecuteCopyOneCommand, copyOneMenuCommandId);
+				commandService.AddCommand(copyOneMenuCommand);
+
+				var copyTwoMenuCommandId = new CommandID(CommandSet, CopyTwoCommandId);
+				var copyTwoMenuCommand = new MenuCommand(this.ExecuteCopyTwoCommand, copyTwoMenuCommandId);
+				commandService.AddCommand(copyTwoMenuCommand);
+			}
+		}
+
+		/// <summary>
+		/// Gets the singleton instance of the command.
+		/// </summary>
+		public static TrimCopyCommand Instance { get; private set; }
+
+		/// <summary>
+		/// Gets the service provider from the owner package.
+		/// </summary>
+		private IServiceProvider ServiceProvider => this.package;
+
+		/// <summary>
+		/// Initializes the singleton instance of the command.
+		/// </summary>
+		/// <param name="package">Owner package, not null.</param>
+		public static void Initialize(Package package)
+		{
+			Instance = new TrimCopyCommand(package);
+		}
+
+		#region Execute
+
+		private void ExecuteCopyOneCommand(object sender, EventArgs e) => GetFormatSetText(true);
+		private void ExecuteCopyTwoCommand(object sender, EventArgs e) => GetFormatSetText(false);
+
+		private void GetFormatSetText(bool isNoIndent)
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			var activeDocument = ((DTE2)Package.GetGlobalService(typeof(DTE))).ActiveDocument;
+			if (activeDocument is null)
+				return;
+
+			var text = GetSelectionText(activeDocument);
+			if (string.IsNullOrEmpty(text))
+				return;
+
+			var tabSize = GetTabSize(activeDocument);
+
+			var formatted = StringFormatter.Format(
+				source: text,
+				tabSize: tabSize,
+				fixedIndentSize: (isNoIndent ? 0 : Settings.Current.FixedIndentSize),
+				trimTrailingSpaces: Settings.Current.TrimTrailingSpaces,
+				useHtmlEncode: Settings.Current.UseHtmlEncode,
+				lineEndType: Settings.Current.LineEndType);
+
+			Clipboard.SetText(formatted); // Exception handling is to be considered.
+		}
+
+		private string GetSelectionText(Document activeDocument)
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			var selection = activeDocument.Selection as TextSelection;
+			if (selection is null)
+				return null;
+
+			var topLine = selection.TopLine;
+			var bottomLine = selection.BottomLine;
+
+			selection.GotoLine(topLine, true);
+			selection.MoveToLineAndOffset(bottomLine, 1, true);
+			selection.EndOfLine(true);
+
+			return selection.Text;
+		}
+
+		private int GetTabSize(Document activeDocument)
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			if (Settings.Current.UseTabSizeInTextEditor)
+			{
+				var value = UserSettings.GetTabSize(ServiceProvider, activeDocument.Language);
+				if (value.HasValue)
+				{
+					Debug.WriteLine($"Language: {activeDocument.Language}, TabSize: {value.Value}");
+					return value.Value;
+				}
+			}
+			return Settings.Current.TabSize;
+		}
+
+		#endregion
+	}
+}
